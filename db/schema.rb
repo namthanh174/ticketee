dbfbda3989ddf83e8d02e2b0da127e290ed121a6
thanh vo <namthanh174@gmail.com>
2018-05-29 10:08:09 +0000

    'Creating a new project' feature complete.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_05_29_094206) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
