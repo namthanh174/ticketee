1833cba41bbacd4d3ef07750e505feaf4d04af88
thanh vo <namthanh174@gmail.com>
2018-06-05 03:28:14 +0000

    Add admin namespace with application controller

---
+++ spec/controllers/admin/application_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Admin::ApplicationController, type: :controller do
+
+  # describe "GET #index" do
+  #   it "returns http success" do
+  #     get :index
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  let(:user) { FactoryBot.create(:user)}
+  
+  before do
+    allow(controller).to receive(:authenticate_user!)
+    allow(controller).to receive(:current_user).and_return(user)
+  end
+  
+  context "non-admin users" do
+    it "are not able to access the index action" do
+      get :index
+      
+      expect(response).to redirect_to '/'
+      expect(flash[:alert]).to eq "You must be an admin to do that."
+    end
+  end
+end
