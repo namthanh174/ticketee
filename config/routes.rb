8100a4662a25823900e6cc97b5c5a43aa3ee05fd
thanh vo <namthanh174@gmail.com>
2018-06-06 07:40:15 +0000

    Add feature for archiving users, including protection against self-archiving

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
     root 'application#index'
     
     resources :projects, only: [:new, :create, :destroy]
-    resources :users
+    resources :users do
+      member do
+        patch :archive
+      end
+    end
   end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
